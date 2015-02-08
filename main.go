1da9a7333f5389d6e97b2d29e8f60582cdb9d684
Radoslav Stankov <rstankov@gmail.com>
2015-02-08 14:27:05 +0200

    Introduce templates

--- main.go
+++ main.go
@@ -1,22 +1,68 @@
 package main
 
 import (
-    "log"
-    "fmt"
-    "net/http"
+  "html/template"
+  "log"
+  "fmt"
+  "net/http"
+  "os"
+  "path"
 )
 
 func handler(w http.ResponseWriter, r *http.Request) {
-    fmt.Fprintf(w, "path - %s", r.URL.Path[1:])
-    fmt.Printf("GET %s\n", r.URL.Path)
+  fmt.Fprintf(w, "path - %s", r.URL.Path[1:])
+  fmt.Printf("GET %s\n", r.URL.Path)
 }
 
 func main() {
-    fs := http.FileServer(http.Dir("static"))
+  fs := http.FileServer(http.Dir("static"))
 
-    log.Println("Listening on 8080...")
+  http.Handle("/static/", http.StripPrefix("/static/", fs))
+  http.HandleFunc("/", serveTemplate)
+  http.HandleFunc("/api/", handler)
 
-    http.Handle("/", fs)
-    http.HandleFunc("/api/", handler)
-    http.ListenAndServe(":8080", nil)
+
+  log.Println("Listening on 8080...")
+  http.ListenAndServe(":8080", nil)
+}
+
+func serveTemplate(w http.ResponseWriter, r *http.Request) {
+  filePath := r.URL.Path
+
+  if filePath == "/" {
+    filePath = "index"
+  }
+  filePath = filePath + ".html"
+
+  lp := path.Join("templates", "layout.html")
+  fp := path.Join("templates", filePath)
+
+  // Return a 404 if the template doesn't exist
+  info, err := os.Stat(fp)
+  if err != nil {
+    if os.IsNotExist(err) {
+      http.NotFound(w, r)
+      return
+    }
+  }
+
+  // Return a 404 if the request is for a directory
+  if info.IsDir() {
+    http.NotFound(w, r)
+    return
+  }
+
+  tmpl, err := template.ParseFiles(lp, fp)
+  if err != nil {
+    // Log the detailed error
+    log.Println(err.Error())
+    // Return a generic "Internal Server Error" message
+    http.Error(w, http.StatusText(500), 500)
+    return
+  }
+
+  if err := tmpl.ExecuteTemplate(w, "layout", nil); err != nil {
+    log.Println(err.Error())
+    http.Error(w, http.StatusText(500), 500)
+  }
 }
